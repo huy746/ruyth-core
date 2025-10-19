278876fc07859ea4bb497aa33559c3dd018f333e
Huy <huyvohoang97@gmail.com>
2025-10-19 18:07:31 +0700

    Update slash.py

--- ruythcore/slash.py
+++ ruythcore/slash.py
@@ -1,1 +1,14 @@
+class SlashManager:
+    def __init__(self, bot):
+        self.bot = bot
+        self.commands = {}
 
+    def slash(self, name, description=""):
+        def decorator(func):
+            self.commands[name] = func
+            return func
+        return decorator
+
+    async def register_global_commands(self):
+        pass
+                                       
