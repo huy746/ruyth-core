aebbd38ab325ecdc36d145a2ca16c654b5036bf0
Huy <218056340+huy746@users.noreply.github.com>
2025-10-24 13:28:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ruythcore",               
-    version="1.0.0",                
-    author="huy746",
-    author_email="huyminehelp@gmail.com",
-    description="A lightweight prefix-based bot core framework",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/huy746/ruyth-core",
+    name="ruyth-core",
+    version="1.0.0",
     packages=find_packages(),
-    install
+    author="huy746",
+    description="Core library for Ruyth Discord bot",
+    url="https://github.com/huy746/ruyth-core",
+    python_requires='>=3.8',
 )
