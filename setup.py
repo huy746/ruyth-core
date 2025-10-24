8308aa757bbacee5741616cefd6d91fd354a9ee9
Huy <218056340+huy746@users.noreply.github.com>
2025-10-24 13:33:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup (
     version="1.0.0",
     packages=find_packages(),
     author="huy746",
-    description="Core library for Ruyth Discord bot",
+    description="This is a library for Discord Bot written in Python",
     url="https://github.com/huy746/ruyth-core",
     python_requires='>=3.8',
 )
