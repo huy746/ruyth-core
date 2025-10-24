3514daade55a47485979ae33f2a9d74585cbda80
Huy <218056340+huy746@users.noreply.github.com>
2025-10-24 13:15:58 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ruythcore",
-    version="0.1.0",
+    name="ruythcore",               
+    version="1.0.0",                
     author="huy746",
+    author_email="huyminehelp@gmail.com",
+    description="A lightweight prefix-based bot core framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/huy746/ruyth-core",
     packages=find_packages(),
-    install_requires=[],
-    python_requires=">=3.7",
-)
+    install
