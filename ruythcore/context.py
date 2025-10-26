a611de4f1f61cc205756a03c620ed75fde28fee0
Huy <218056340+huy746@users.noreply.github.com>
2025-10-26 18:43:54 +0700

    Update context.py

--- ruythcore/context.py
+++ ruythcore/context.py
@@ -1,10 +1,11 @@
 class Context:
-    def __init__(self, bot, raw_event):
-        self.bot = bot
+    def __init__(self, client, raw_event):
+        self.client = client
         self.raw = raw_event
-        self.channel_id = raw_event["channel_id"]
-        self.author = raw_event["author"]["username"]
+        self.channel_id = raw_event.get("channel_id")
+        self.author = raw_event.get("author", {}).get("username")
 
     async def reply(self, content: str):
-        await self.bot.http.send_message(self.channel_id, content)
-      
+        """Send a message to the channel where event came from."""
+        await self.client.http.send_message(self.channel_id, content)
+        
