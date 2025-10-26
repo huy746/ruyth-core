55b9bc54668074e2ca0138c618e89d26c6017efc
Huy <218056340+huy746@users.noreply.github.com>
2025-10-26 18:51:05 +0700

    Update client.py

--- ruythcore/client.py
+++ ruythcore/client.py
@@ -3,25 +3,28 @@ from .http import HTTPClient
 from .gateway import Gateway
 from .command import PrefixCommandManager
 from .slash import SlashManager
+from .voice import VoiceManager
+from .utils import ensure_task
 from .constants import DEFAULT_INTENTS
 
-class RuythBot:
-    def __init__(self, token, prefix="!", intents=None):
+class Client:
+    def __init__(self, token: str, prefix: str = "!"):
         self.token = token
         self.prefix = prefix
-        self.intents = intents or DEFAULT_INTENTS
-
         self.http = HTTPClient(token)
-        self.gateway = Gateway(token, self.intents, self._dispatch)
-        self.prefix_manager = PrefixCommandManager(self)
+        self.gateway = Gateway(token, DEFAULT_INTENTS, self._dispatch)
+        self.commands = PrefixCommandManager(self)
         self.slash = SlashManager(self)
+        self.voice = VoiceManager(self)
         self._events = {}
+        self.user = None
 
-    def command(self, name):
-        return self.prefix_manager.command(name)
+    # decorators
+    def command(self, name: str = None):
+        return self.commands.command(name)
 
-    def slash(self, name, description=""):
-        return self.slash.slash(name, description)
+    def slash_command(self, name: str, description: str = ""):
+        return self.slash.command(name, description)
 
     def event(self, fn):
         self._events[fn.__name__] = fn
@@ -29,21 +32,33 @@ class RuythBot:
 
     async def _dispatch(self, ev_name, data):
         if ev_name == "on_ready":
-            await self.slash.register_global_commands()
+            # set user if present
+            try:
+                self.user = data.get("user") or data.get("user", {})
+            except Exception:
+                pass
+            # register slash commands when ready
+            try:
+                await self.slash.register_global_commands()
+            except Exception:
+                pass
+
         if ev_name == "on_message":
-            await self.prefix_manager.handle_message(data)
+            await self.commands.handle_message(data)
+
         if ev_name == "on_interaction":
-            name = data.get("data", {}).get("name")
-            if name in self.slash.commands:
-                await self.slash.commands[name](data)
+            await self.slash.handle_interaction(data)
+
         if ev_name in self._events:
             await self._events[ev_name](data)
 
     async def start(self):
         await self.gateway.connect()
 
-    def launch(self):
+    def run(self):
         asyncio.run(self.start())
 
-    run = launch
-            
+class RuythCore(Client):
+    """Alias class name for branding"""
+    pass
+    
