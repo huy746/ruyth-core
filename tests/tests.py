85f8d9fdde5f6d31c0936f25604cb1c2b02ac035
Huy <218056340+huy746@users.noreply.github.com>
2025-10-26 09:12:25 +0700

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,15 @@
+import ruythcore
+
+bot = ruythcore.RuythBot("TOKEN")
+
+@bot.event
+async def on_ready(data):
+    print("✅ Bot sẵn sàng!")
+
+@bot.command("ping")
+async def ping(data):
+    ctx = ruythcore.Context(bot, data["d"])
+    await ctx.reply("Pong!")
+
+bot.launch()
 
