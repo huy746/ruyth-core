f07b74d5584f08a6dc9c63bd79cd12f7a8ba4f2f
Huy <218056340+huy746@users.noreply.github.com>
2025-10-26 18:48:29 +0700

    Update _init_.py

--- ruythcore/_init_.py
+++ ruythcore/_init_.py
@@ -1,4 +1,24 @@
-from .client import RuythBot
-from .utils import version
+from .client import Client, RuythCore
+from .context import Context
+from .slash import SlashManager
+from .command import PrefixCommandManager
+from .http import HTTPClient
+from .voice import VoiceManager
+from .constants import DEFAULT_INTENTS
 
-__all__ = ["RuythBot", "version"]
+# Expose names at package level so users can:
+# import ruythcore
+# bot = ruythcore.Client("token")
+__all__ = [
+    "Client",
+    "RuythCore",
+    "Context",
+    "SlashManager",
+    "PrefixCommandManager",
+    "HTTPClient",
+    "VoiceManager",
+    "DEFAULT_INTENTS",
+]
+
+__version__ = "1.0.0"
+__author__ = "huy746"
