cd54509941898ff45ad38a9356dde830815893ad
Huy <218056340+huy746@users.noreply.github.com>
2025-10-26 18:55:34 +0700

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,8 @@
-def test_basic():
-    assert True
+import ruythcore
+
+def test_imports():
+    c = ruythcore.Client("fake-token")
+    assert hasattr(c, "http")
+    assert hasattr(c, "slash")
+    assert hasattr(c, "voice")
+    
