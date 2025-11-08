4c3a0fc7240cb4cdfb37a469c7b6d09859957e98
Huy <218056340+huy746@users.noreply.github.com>
2025-11-08 16:42:24 +0700

    Update test_dummy.py

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -6,9 +6,6 @@ repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if repo_root not in sys.path:
     sys.path.insert(0, repo_root)
 
-# In debug xem import từ đâu
-import Client from ruythcore
-print(">>> DEBUG: ruythcore được import từ:", getattr(ruythcore, "__file__", "Không có __file__"))
 
 # Nếu không có attribute Client -> ép reload từ đúng file __init__.py
 if not hasattr(ruythcore, "Client"):
