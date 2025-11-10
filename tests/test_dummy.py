1a3d60b751edb6858db372e7f7b535f5c198e38f
Huy <218056340+huy746@users.noreply.github.com>
2025-11-10 17:47:54 +0700

    Update test_dummy.py

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,8 @@
+import asyncio
 from ruythcore.client import Client
 
 def test_imports():
+    asyncio.set_event_loop(asyncio.new_event_loop())  # <-- thêm dòng này
     c = Client("fake-token")
     assert hasattr(c, "http")
     assert hasattr(c, "slash")
@@ -9,3 +11,4 @@ def test_imports():
 if __name__ == "__main__":
     test_imports()
     print(">>> Test import Client thành công!")
+    
