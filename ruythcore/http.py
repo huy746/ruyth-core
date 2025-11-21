57c2a694e63f180faa880c86073942f99e8e2224
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:27:29 +0700

    Update http.py

--- ruythcore/http.py
+++ ruythcore/http.py
@@ -1,13 +1,24 @@
 import aiohttp
+from .constants import HTTP_BASE, USER_AGENT
 
 class HTTPClient:
-    def __init__(self, token: str):
+    def __init__(self, token):
         self.token = token
-        self.session = aiohttp.ClientSession(headers={
-            "Authorization": f"Bot {token}"
-        })
+        self.session = aiohttp.ClientSession()
 
-    async def request(self, method: str, url: str, **kwargs):
-        async with self.session.request(method, url, **kwargs) as r:
-            return await r.json()
-            
+    async def request(self, method, path, **kwargs):
+        headers = kwargs.pop("headers", {})
+        headers.update({
+            "Authorization": f"Bot {self.token}",
+            "User-Agent": USER_AGENT
+        })
+        async with self.session.request(method, HTTP_BASE + path, headers=headers, **kwargs) as resp:
+            if resp.status >= 400:
+                raise Exception(await resp.text())
+            try:
+                return await resp.json()
+            except:
+                return await resp.text()
+
+    async def close(self):
+        await self.session.close()
