e217b02a9c2633ed34d0ea152562e5aa3cc6c35b
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:23:39 +0700

    Update voice.py

--- ruythcore/voice.py
+++ ruythcore/voice.py
@@ -1,3 +1,6 @@
-class VoiceManager:
-    def __init__(self, client):
-        self.client = client
+class VoiceClient:
+    def __init__(self):
+        self.ready = False
+
+    async def connect(self):
+        self.ready = True
