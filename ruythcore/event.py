3962fc43cfa3d1d9d4e459ee6e46e14f5479d7f8
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:26:54 +0700

    Update event.py

--- ruythcore/event.py
+++ ruythcore/event.py
@@ -1,1 +1,14 @@
+class EventHandler:
+    def __init__(self):
+        self.listeners = {}
 
+    def on(self, name):
+        def decorator(func):
+            self.listeners.setdefault(name, []).append(func)
+            return func
+        return decorator
+
+    async def emit(self, name, *args, **kwargs):
+        for func in self.listeners.get(name, []):
+            await func(*args, **kwargs)
+                                      
