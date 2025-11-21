2abab293c4d24cc61d2ffdf3b574860d5e12c542
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:29:46 +0700

    Create models.py

---
+++ ruythcore/models.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Dict, Any
+
+@dataclass
+class User:
+    id: str
+    username: str
+    discriminator: str = '0000'
+    bot: bool = False
+
+@dataclass
+class Message:
+    id: str
+    channel_id: str
+    content: str
+    author: User
+    raw: Dict[str, Any] = None
+  
