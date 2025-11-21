37e60635de41fb338ff97c9efa9752d96ba79fc4
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:54:12 +0700

    Update __init__.py

--- ruythcore/__init__.py
+++ ruythcore/__init__.py
@@ -1,11 +1,3 @@
-"""
-ruythcore - A simple Discord bot library
-"""
-
-__version__ = "1.0.2"
-__author__ = "ruythbot_huy"
-
-# Import core classes
 from .client import Client
 from .commands import CommandManager
 from .slash import SlashCommandManager
@@ -15,33 +7,23 @@ from .gateway import Gateway
 from .events import EventHandler
 from .context import Context
 from .constants import *
-from . import utils  # Import utils.py
+from . import utils
 
-# Define what is exported when doing `from ruythcore import *`
+# Aliases chuẩn
+Command = CommandManager
+SlashCommand = SlashCommandManager
+
 __all__ = [
     "Client",
-    "CommandManager",
-    "SlashCommandManager",
+    "Command",
+    "SlashCommand",
     "VoiceClient",
     "HTTPClient",
     "Gateway",
     "EventHandler",
     "Context",
-    "utils",
-    "GATEWAY_URL",
-    "HTTP_BASE",
-    "USER_AGENT",
-    "INTENTS_ALL",
-    "OP_DISPATCH",
-    "OP_HEARTBEAT",
-    "OP_IDENTIFY",
-    "OP_STATUS_UPDATE",
-    "OP_VOICE_STATE_UPDATE",
-    "OP_VOICE_SERVER_PING",
-    "OP_RESUME",
-    "OP_RECONNECT",
-    "OP_REQUEST_GUILD_MEMBERS",
-    "OP_INVALID_SESSION",
-    "OP_HELLO",
-    "OP_HEARTBEAT_ACK"
+    "utils"
 ]
+
+__version__ = "1.0.2"
+__author__ = "ruythbot_huy"
