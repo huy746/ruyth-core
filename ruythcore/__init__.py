fa51ac9d47b0b5ad72a8db05a67851639d6c1f58
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:37:40 +0700

    Update __init__.py

--- ruythcore/__init__.py
+++ ruythcore/__init__.py
@@ -1,35 +1,47 @@
-# ruythcore/__init__.py
+"""
+ruythcore - A simple Discord bot library
+"""
 
-from .client import Client, RuythCore
-from .context import Context
-from .slash import SlashManager
-from .command import PrefixCommandManager
+__version__ = "1.0.2"
+__author__ = "ruythbot_huy"
+
+# Import core classes
+from .client import Client
+from .commands import CommandManager
+from .slash import SlashCommandManager
+from .voice import VoiceClient
 from .http import HTTPClient
-from .voice import VoiceManager
-from .constants import DEFAULT_INTENTS
+from .gateway import Gateway
+from .events import EventHandler
+from .context import Context
+from .constants import *
+from . import utils  # Import utils.py
 
+# Define what is exported when doing `from ruythcore import *`
 __all__ = [
     "Client",
-    "RuythCore",
-    "Context",
-    "SlashManager",
-    "PrefixCommandManager",
+    "CommandManager",
+    "SlashCommandManager",
+    "VoiceClient",
     "HTTPClient",
-    "VoiceManager",
-    "DEFAULT_INTENTS",
+    "Gateway",
+    "EventHandler",
+    "Context",
+    "utils",
+    "GATEWAY_URL",
+    "HTTP_BASE",
+    "USER_AGENT",
+    "INTENTS_ALL",
+    "OP_DISPATCH",
+    "OP_HEARTBEAT",
+    "OP_IDENTIFY",
+    "OP_STATUS_UPDATE",
+    "OP_VOICE_STATE_UPDATE",
+    "OP_VOICE_SERVER_PING",
+    "OP_RESUME",
+    "OP_RECONNECT",
+    "OP_REQUEST_GUILD_MEMBERS",
+    "OP_INVALID_SESSION",
+    "OP_HELLO",
+    "OP_HEARTBEAT_ACK"
 ]
-
-# Attach module-level để import ruythcore.Client hoạt động
-import sys
-_module = sys.modules[__name__]
-_module.Client = Client
-_module.RuythCore = RuythCore
-_module.PrefixCommandManager = PrefixCommandManager
-_module.Context = Context
-_module.SlashManager = SlashManager
-_module.HTTPClient = HTTPClient
-_module.VoiceManager = VoiceManager
-_module.DEFAULT_INTENTS = DEFAULT_INTENTS
-
-__version__ = "1.0.1"
-__author__ = "ruythbot_huy"
