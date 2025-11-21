f0a1ffc3e6eeeba1252c871bcc17dc47edf61585
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:30:50 +0700

    Update context.py

--- ruythcore/context.py
+++ ruythcore/context.py
@@ -1,7 +1,14 @@
 class Context:
-    def __init__(self, message=None, interaction=None):
-        self.message = message
-        self.interaction = interaction
+    def __init__(self, client, msg):
+        self.client = client
+        self.message = msg
+        self.channel_id = getattr(msg, "channel_id", None)
 
-    async def send(self, content):
-        print(f"[BOT SEND]: {content}")
+    async def reply(self, content: str):
+        if self.channel_id:
+            return await self.client.http.request(
+                "POST",
+                f"/channels/{self.channel_id}/messages",
+                json={"content": content},
+            )
+                
