9127386cbd2405735f9bfa5ad10f8cb50c345980
Huy <218056340+huy746@users.noreply.github.com>
2025-11-21 12:24:18 +0700

    Update gateway.py

--- ruythcore/gateway.py
+++ ruythcore/gateway.py
@@ -1,22 +1,42 @@
-
-import asyncio
-import websockets
-from .constants import GATEWAY_URL
+import asyncio, json, websockets
+from .constants import GATEWAY_URL, OP_HEARTBEAT, OP_IDENTIFY
 
 class Gateway:
     def __init__(self, token, intents, dispatch):
         self.token = token
         self.intents = intents
-        self._dispatch = dispatch
+        self.dispatch = dispatch
         self.ws = None
+        self.heartbeat_interval = 0
+        self.seq = None
 
     async def connect(self):
-        async with websockets.connect(GATEWAY_URL) as ws:
-            self.ws = ws
-            await self._run()
+        self.ws = await websockets.connect(GATEWAY_URL)
+        hello = json.loads(await self.ws.recv())
+        self.heartbeat_interval = hello['d']['heartbeat_interval'] / 1000
+        await self.identify()
+        asyncio.create_task(self.heartbeat())
+        asyncio.create_task(self.listen())
 
-    async def _run(self):
-        async for message in self.ws:
-            # Fake dispatch
-            await self._dispatch("on_ready", {"user": {"username": "RuythBot"}})
-    
+    async def identify(self):
+        await self.ws.send(json.dumps({
+            'op': OP_IDENTIFY,
+            'd': {
+                'token': self.token,
+                'intents': self.intents,
+                'properties': {'$os':'linux','$browser':'ruythcore','$device':'ruythcore'}
+            }
+        }))
+
+    async def heartbeat(self):
+        while True:
+            await asyncio.sleep(self.heartbeat_interval)
+            await self.ws.send(json.dumps({'op': OP_HEARTBEAT,'d':self.seq}))
+
+    async def listen(self):
+        async for msg in self.ws:
+            data = json.loads(msg)
+            if data.get('s'):
+                self.seq = data['s']
+            if data.get('t'):
+                await self.dispatch(data['t'], data['d'])
